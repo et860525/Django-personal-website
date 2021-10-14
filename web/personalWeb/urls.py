ff0d2769d3d6db910faea4a4888bdfa192633289
et860525 <et860525@gmail.com>
2021-10-14 17:18:10 +0800

    Add views, add "project.html" and static files
    views, urls
      Add views
      - project
      - blog
      - contact
      - about
    
    project.html
      Show project
      - Blog
      - Todo
    
      Two picture added

--- web/personalWeb/urls.py
+++ web/personalWeb/urls.py
@@ -3,4 +3,8 @@ from personalWeb import views
 
 urlpatterns = [
 	path('', views.home_page, name='home_page'),
+	path('project/', views.project_page, name='project_page'),
+	path('blog/', views.blog_page, name='blog_page'),
+	path('contact/', views.contact_page, name='contact_page'),
+	path('about/', views.about_page, name='about_page')
 ]
--- web/personalWeb/views.py
+++ web/personalWeb/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
 	return render(request, 'home.html')
+
+def project_page(request):
+	return render(request, 'project.html')
+
+def blog_page(request):
+	pass
+
+def contact_page(request):
+	pass
+
+def about_page(request):
+	pass

54a7ec527f350880d96012a1791f5126af27f5b4
et860525 <et860525@gmail.com>
2022-02-08 20:14:57 +0800

    Markdown syntax highlight system
    Change: For syntax highlight.
    
    Change list:
    1. Modify `blog_extras.py`:
      - add new extensions 'codehilite' to parse syntax blocks
    2. Install `Pygments` and add `github.css`:
      - Highlight syntax
    3. Add markdown css

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -8,4 +8,8 @@ register = template.Library()
 @register.filter
 @stringfilter
 def convert_markdown(value):
-	return markdown.markdown(value, extensions=['markdown.extensions.fenced_code'])
+	return markdown.markdown(value, 
+							extensions=[
+								'markdown.extensions.fenced_code',
+								'markdown.extensions.codehilite'
+							])
